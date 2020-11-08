1a0d1603f5bd69489a880dbc6ed45179aedc7e98
Leo Zhang <leoucfstudent@knights.ucf.edu>
2020-11-07 19:16:25 -0500

    upload

---
+++ Assets/Scripts/Scriptable Objects/StateData.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Game.PlayerCharacter
+{
+    /// <summary>
+    /// the base model for every player action/ability that has
+    /// and will be created.
+    /// Always get the player movement script from the CharacterStateBase class
+    /// </summary>
+    public abstract class StateData : ScriptableObject
+    {
+        public abstract void OnEnter(CharacterStateBase character, Animator a, AnimatorStateInfo asi);
+        public abstract void UpdateAbility(CharacterStateBase c, Animator a, AnimatorStateInfo asi);
+        public abstract void OnExit(CharacterStateBase c, Animator a, AnimatorStateInfo asi);
+    }
+}
