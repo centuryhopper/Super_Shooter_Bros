99a00308b01e33a69aa38a5b7cd1d0901ff8fd3c
Leo Zhang <leotheasianlion@gmail.com>
2021-01-05 02:38:04 -0500

    animation rigging progress

--- Assets/Scripts/Player/PointExample.cs
+++ Assets/Scripts/Player/PointExample.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations.Rigging;
+// using UnityEngine.Input
 
 public class PointExample : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Header("References"), SerializeField]
+    Rig rig = null;
+
+    [Header("Settings"), SerializeField]
+    float pointSpeed = 1f;
+
+    int targetValue;
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            targetValue = targetValue == 0 ? 1 : 0;
+        }
+
+        rig.weight = Mathf.MoveTowards(rig.weight, targetValue, pointSpeed * Time.deltaTime);
     }
 }
