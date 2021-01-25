bb9f177c528a73ebb668de5c07f8c61f86637b5e
Leo Zhang <leotheasianlion@gmail.com>
2021-01-25 01:06:59 -0500

    added training agent scene for machine learning

---
+++ Assets/Scripts/Enemy/BouncerTarget.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.MLAgents;
+using UnityEngine;
+
+public class BouncerTarget : MonoBehaviour
+{
+    void FixedUpdate()
+    {
+        gameObject.transform.Rotate(new Vector3(1, 0, 0), 0.5f);
+    }
+
+    void OnTriggerEnter(Collider collision)
+    {
+        var agent = collision.gameObject.GetComponent<Agent>();
+        if (agent != null)
+        {
+            agent.AddReward(1f);
+            Respawn();
+        }
+    }
+
+    public void Respawn()
+    {
+        gameObject.transform.localPosition =
+            new Vector3(
+                (1 - 2 * Random.value) * 5f,
+                2f + Random.value * 5f,
+                (1 - 2 * Random.value) * 5f);
+    }
+}
