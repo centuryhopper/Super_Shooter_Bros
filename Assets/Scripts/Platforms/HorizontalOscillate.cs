d486d4d6f4e068fada4b89469eb003c2b5ad7d2e
Leo Zhang <leotheasianlion@gmail.com>
2021-02-26 00:17:51 -0500

    updates
    introduced horizontal and vertical moving platforms in game

--- Assets/Scripts/Platforms/HorizontalOscillate.cs
+++ Assets/Scripts/Platforms/HorizontalOscillate.cs
@@ -1,51 +1,56 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class HorizontalOscillate : MonoBehaviour
+namespace Game.Plaforms
 {
-    Transform t;
-    [SerializeField, Range(0,5f), Tooltip("The range of oscillation")] float amplitude;
-    [SerializeField, Range(0,25f), Tooltip("How fast the platform will oscillate")] float frequency;
-    [SerializeField, Range(0,100f)] float offset = 5f;
-
-    // Start is called before the first frame update
-    void Start()
+    public class HorizontalOscillate : MonoBehaviour
     {
-        t = transform;
-    }
+        Transform t;
+        float startingZ;
+        [SerializeField, Range(0, 5f), Tooltip("The range of oscillation")] float amplitude;
+        [SerializeField, Range(0, 25f), Tooltip("How fast the platform will oscillate")] float frequency;
+        [SerializeField, Range(0, 10f)] float offset = 5f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        Oscillate();
-    }
 
-    private void Oscillate()
-    {
-        float x = t.position.x;
-        float y = t.position.y;
-        float z = t.position.z;
-        z = offset + amplitude * Mathf.Cos(frequency * Time.timeSinceLevelLoad);
-        t.position = new Vector3(x, y, z);
-    }
-
-    void OnCollisionEnter(Collision collisionInfo)
-    {
-        if (collisionInfo.transform.CompareTag("Player"))
+        // Start is called before the first frame update
+        void Start()
         {
-            collisionInfo.transform.SetParent(this.transform);
+            t = transform;
+            startingZ = t.position.z;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            Oscillate();
+        }
+
+        private void Oscillate()
+        {
+            float x = t.position.x;
+            float y = t.position.y;
+            float z = t.position.z;
+
+            // properly offset the correctly axis for good oscillation
+            z = startingZ + offset + amplitude * Mathf.Cos(frequency * Time.timeSinceLevelLoad);
+            t.position = new Vector3(x, y, z);
+        }
+
+        void OnCollisionEnter(Collision collisionInfo)
+        {
+            if (collisionInfo.transform.CompareTag("Player"))
+            {
+                // set player parent to this platorm so that the player will be moved
+                // by the platform it is currently on
+                collisionInfo.transform.SetParent(this.transform);
+            }
+        }
+
+        void OnCollisionExit(Collision collisionInfo)
+        {
+            if (collisionInfo.transform.CompareTag("Player"))
+            {
+                collisionInfo.transform.SetParent(null);
+            }
         }
     }
-
-    void OnCollisionExit(Collision collisionInfo)
-    {
-        if (collisionInfo.transform.CompareTag("Player"))
-        {
-            collisionInfo.transform.SetParent(null);
-        }
-    }
-
-
-
 }
