717c020095e090161890dbde8620f88690a98d73
Leo Zhang <leotheasianlion@gmail.com>
2021-07-23 19:08:57 -0400

    updates
    added alert dialog for proceeding to next level before destroying all enemies

--- Assets/Scripts/Level Design scripts/TriggeredPortal.cs
+++ Assets/Scripts/Level Design scripts/TriggeredPortal.cs
@@ -4,12 +4,23 @@ using UnityEngine;
 using Game.portal_manager;
 using Game.Spawner;
 using Game.EnemyAI;
-
+using TMPro;
 
 namespace Game.LevelDesign
 {
     public class TriggeredPortal : MonoBehaviour
     {
+        private Coroutine showAlertRoutine = null;
+        [SerializeField] float showAlertMsgTime = 5f;
+        private WaitForSeconds wait;
+        [SerializeField] GameObject alertMsg = null;
+
+        void Start()
+        {
+            wait = new WaitForSeconds(showAlertMsgTime);
+            alertMsg.SetActive(false);
+        }
+
         void OnTriggerEnter(Collider other)
         {
             // only call this when all enemies in the scene are deactivated
@@ -17,6 +28,22 @@ namespace Game.LevelDesign
             {
                 PortalManager.instance.loadDesiredScene();
             }
+            else
+            {
+                // show alert message
+                if (showAlertRoutine != null) StopCoroutine(showAlertRoutine);
+                showAlertRoutine = StartCoroutine(showAlert());
+            }
         }
+
+        private IEnumerator showAlert()
+        {
+            alertMsg.SetActive(true);
+            yield return wait;
+            alertMsg.SetActive(false);
+        }
+
+
+
     }
 }
